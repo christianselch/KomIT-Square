0cd993f7553e4f75a4b4d73683cd01366c13b723
Christian Selch <christian@selch.me>
2013-03-19 17:24:35 +0100

    Added footer - Tweaked mobile view
    Moved contact information above body - more important on the go

--- js/main.js
+++ js/main.js
@@ -7,8 +7,17 @@
 			prevText: "Forrige",
 			nextText: "Next",
 		});
+
+		$(".contactMail").html('<a href="mailto:Square Te - Virum <square@mail.dk>">sqare@mail.dk</a>');
+
+		$(window).on("resize", function(){
+	        if($(window).width() > 600)
+	        {
+	            $("#contactSidebar").remove().insertAfter($("#main"));
+	        } else {
+	            $("#contactSidebar").remove().insertBefore($("#main"));
+	        }
+		})
+		$(window).trigger("resize");
 	});
 })();
-
-
-
